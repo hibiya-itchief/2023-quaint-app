b5e797e09aac22711ff84224e37ffb9d05883b08
Ekke-kuru2 <71656810+Ekke-kuru2@users.noreply.github.com>
2022-07-24 12:07:26 +0900

    fontを変更 デフォルトがRobotoで日本語フォントではなかったので、Noto sansに変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,8 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {rel:'stylesheet',href:'https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap'}
     ]
   },
 
@@ -97,6 +98,7 @@ export default {
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
+    treeShake:true,
     theme: {
       dark: false,
       themes: {
