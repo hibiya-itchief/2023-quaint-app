c0e0a9bbe22238160c8bf8650c6a67629a6ece06
TJ <yj_ykt@icloud.com>
2022-08-17 11:39:50 +0900

    Added QR Code Reader feature in auth/ticket/index.vue

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,6 +34,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: '~/plugins/vue-qrcode-reader', ssr: false }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
---
+++ pages/auth/ticket/index.vue
@@ -0,0 +1,123 @@
+<template>
+<v-app >
+  <v-container>
+    <div justify="center">
+      <v-btn elevation="0" v-if="!readCode" @click="openCodeReader">QR コードを読み取る</v-btn>
+
+      <div v-if="readCode">
+        <p class="error">{{ error }}</p>
+        <qrcode-stream @decode="onDecode" @init="onInit" />
+      </div>
+
+      <div v-if="!readCode">
+        <li v-for="(event, i) in events" :key="event.id">
+          {{ event.id}}
+          <v-btn v-bind:disabled="event.is_used" elevation="0" color="primary" @click="useTicket(i)">使用済みにする</v-btn>
+        </li>
+      </div>
+    </div>
+  </v-container>
+</v-app>
+</template>
+
+<script>
+export default {
+  name: 'QRCodeReader',
+  auth: false,
+  data() {
+    return {
+      readCode: false, 
+      jwt: '',
+      error: '',
+      events: []
+    }
+  },
+  methods: {
+    openCodeReader () {
+      this.readCode = true
+    },
+    onDecode (decodeResult) {
+      this.jwt = decodeResult
+      this.readCode = false
+      this.events = []
+      for (let i = 0; i < 4 + Math.floor(Math.random() * 3); i++) {
+        this.events.push({
+          event_id: "event id" + String(i),
+          owner_id: "owner_id",
+          is_family_ticket: false,
+          person: 1,
+          id: "id" + String(i),
+          group_id: "groupd_id" + String(i),
+          created_at: "2022-08-11T08:20:58.1167",
+          is_used: Math.random() > 0.5 ? true : false
+        })
+      }
+      /*$axios.post("/auth/ticket", {jwt: decodeResult})
+        .then((result) => {
+          this.events = result.data
+        })
+        .catch((e => {
+          error({ message: e.message })
+        }))*/
+    },
+    useTicket (index) {
+      alert('using index: ' + String(index))
+      this.events[index].is_used = true
+      var event = this.events[index]
+      /*var endPoint = "/groups/" + event.group_id + "/events/" + event.event_id + "/tickets/" + event.id + "is_used"
+      $axios.put(endPoint, {})
+        .then((result) => {
+          this.events[index].is_used = true
+        })
+        .catch((e => {
+          error({ message: e.message })
+        }))*/
+    },
+    async onInit (promise) {
+      try {
+        await promise
+      } catch (error) {
+        if (error.name === 'NotAllowedError') {
+          this.error = "ERROR: you need to grant camera access permission"
+        } else if (error.name === 'NotFoundError') {
+          this.error = "ERROR: no camera on this device"
+        } else if (error.name === 'NotSupportedError') {
+          this.error = "ERROR: secure context required (HTTPS, localhost)"
+        } else if (error.name === 'NotReadableError') {
+          this.error = "ERROR: is the camera already in use?"
+        } else if (error.name === 'OverconstrainedError') {
+          this.error = "ERROR: installed cameras are not suitable"
+        } else if (error.name === 'StreamApiNotSupportedError') {
+          this.error = "ERROR: Stream API is not supported in this browser"
+        } else if (error.name === 'InsecureContextError') {
+          this.error = 'ERROR: Camera access is only permitted in secure context. Use HTTPS or localhost rather than HTTP.';
+        } else {
+          this.error = `ERROR: Camera error (${error.name})`;
+        }
+      }
+    }
+  }
+  /*asyncData({ params, error,$axios }) {
+    return $axios.get("/groups")
+      .then((res) => {
+        return { groups: res.data }
+      })
+      //エラー処理
+      .catch((e => {
+        error({ message: e.message })
+      }))
+  }*/
+}
+</script>
+
+<style>
+.group-title{
+  height:1.3em;
+  padding:auto 0;
+}
+.group-description{
+  clear:both;
+  height:100px;
+}
+
+</style>
