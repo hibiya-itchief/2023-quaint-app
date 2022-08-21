fd8e208e133451578dd5bacddd923cbd8f29649b
ItsukiKigoshi <itsukikigoshi@gmail.com>
2022-08-21 21:41:54 +0900

    Added layout in /pages/tickets/index.vue

--- pages/tickets/index.vue
+++ pages/tickets/index.vue
@@ -4,83 +4,93 @@
             <v-icon>mdi-ticket</v-icon>
             整理券
         </h1>
-        <v-card
-        outlined
-        v-for="user in users"
-        >      
-          <v-card-title class="text-h5">
-            <v-icon>mdi-account-circle</v-icon>
-            username: {{user.username}}
-          </v-card-title>
-          <v-img src="/image/example_qrcode.png"></v-img>
-        </v-card>
-        <div class="my-3"/>
-        <v-tabs
-         centered>
-          <v-tab>
-            未使用
-          </v-tab>
-           <v-tab>
-            使用済み（投票券）
-          </v-tab>
-          
-          <div class="my-2"/>
-          <!--未使用整理券-->
-          <v-tab-item>
-            <div v-for="group in groups">
-              <v-card 
-                class="mx-auto"
-                maxwidth="100%"
-                outlined
-                rounded
-                elevation="2"
-              >
-                <v-list-item two-line>
-                    <v-img  
-                      :src="group.thumbnail" 
-                      max-width="40"
-                      style="margin: 6px"></v-img>
-                    <v-list-item-content>
-                      <v-list-item-title  class="text-h7 mb-1">{{group.title}}</v-list-item-title>
-                      <v-list-item-subtitle>{{group.groupname}}</v-list-item-subtitle>
-                    </v-list-item-content>
-                    <v-btn>
-                      <v-icon>mdi-close</v-icon>
-                      <v-text>取消</v-text>
-                    </v-btn>
-                </v-list-item>
+        <v-conteiner>
+          <v-row>
+            <v-col col="6" md="6" sm="12">
+              <v-card
+              outlined
+              v-for="user in users"
+              >      
+                <v-card-title class="text-h5">
+                  <v-icon>mdi-account-circle</v-icon>
+                  username: {{user.username}}
+                </v-card-title>
+                <v-avatar tile color="primary" size="300"></v-avatar>
               </v-card>
-              <div class="my-2"/>
-            </div>
-          </v-tab-item>
-          <v-tab-item>
-            <div v-for="group in groups">
-              <v-card 
-                class="mx-auto"
-                maxwidth="100%"
-                outlined
-                rounded
-                elevation="2"
-              >
-                <v-list-item two-line>
-                    <v-img  
-                      :src="group.thumbnail" 
-                      max-width="40"
-                      style="margin: 6px"></v-img>
-                    <v-list-item-content >
-                      <v-list-item-title  class="text-h7 mb-1">{{group.title}}</v-list-item-title>
-                      <v-list-item-subtitle>{{group.groupname}}</v-list-item-subtitle>
-                    </v-list-item-content>
-                    <v-btn>
-                      <v-icon>mdi-vote</v-icon>
-                      <v-text>投票</v-text>
-                    </v-btn>
-                </v-list-item>
-              </v-card>
-              <div class="my-2"/>
-            </div>
-          </v-tab-item>
-        </v-tabs>
+            </v-col>
+            <v-col col="6" md="6" sm="12">
+              <div class="my-3"/>
+                <v-tabs
+                centered>
+                  <v-tab>
+                    未使用
+                  </v-tab>
+                  <v-tab>
+                    使用済み（投票券）
+                  </v-tab>
+                  
+                  <div class="my-2"/>
+                  <!--未使用整理券-->
+                  <v-tab-item>
+                    <div v-for="group in groups">
+                      <v-card 
+                        class="mx-auto"
+                        maxwidth="100%"
+                        outlined
+                        rounded
+                        elevation="2"
+                      >
+                        <v-list-item two-line>
+                            <v-img  
+                              :src="group.thumbnail" 
+                              max-width="40"
+                              style="margin: 6px"></v-img>
+                            <v-list-item-content>
+                              <v-list-item-title  class="text-h7 mb-1">{{group.title}}</v-list-item-title>
+                              <v-list-item-subtitle>{{group.groupname}}</v-list-item-subtitle>
+                            </v-list-item-content>
+                            <v-btn>
+                              <v-icon>mdi-close</v-icon>
+                              <v-text>取消</v-text>
+                            </v-btn>
+                        </v-list-item>
+                      </v-card>
+                      <div class="my-2"/>
+                    </div>
+                  </v-tab-item>
+                  <v-tab-item>
+                    <div v-for="group in groups">
+                      <v-card 
+                        class="mx-auto"
+                        maxwidth="100%"
+                        outlined
+                        rounded
+                        elevation="2"
+                      >
+                        <v-list-item two-line>
+                            <v-img  
+                              :src="group.thumbnail" 
+                              max-width="40"
+                              style="margin: 6px"></v-img>
+                            <v-list-item-content >
+                              <v-list-item-title  class="text-h7 mb-1">{{group.title}}</v-list-item-title>
+                              <v-list-item-subtitle>{{group.groupname}}</v-list-item-subtitle>
+                            </v-list-item-content>
+                            <v-btn>
+                              <v-icon>mdi-vote</v-icon>
+                              <v-text>投票</v-text>
+                            </v-btn>
+                        </v-list-item>
+                      </v-card>
+                      <div class="my-2"/>
+                    </div>
+                  </v-tab-item>
+                </v-tabs>
+            </v-col>
+          </v-row>
+        </v-conteiner>
+        
+        
         
     </v-app>
 </template>
@@ -116,6 +126,66 @@
           },
           {
             groupname: '38R',
+            title: 'アナと雪の女王',
+            description: 'string',
+            page_content: 'string',
+            enable_vote: true,
+            twitter_url: 'https://twitter.com/HibiyaSanro',
+            instagram_url: 'https://www.instagram.com/hibiyasanro/',
+            stream_url: 'example.com',
+            id: 'string',
+            thumbnail: '/image/Top-Gun.jpg',
+          },
+           {
+            groupname: '31R',
+            title: 'アナと雪の女王',
+            description: 'string',
+            page_content: 'string',
+            enable_vote: true,
+            twitter_url: 'https://twitter.com/HibiyaSanro',
+            instagram_url: 'https://www.instagram.com/hibiyasanro/',
+            stream_url: 'example.com',
+            id: 'string',
+            thumbnail: '/image/Top-Gun.jpg',
+          },
+           {
+            groupname: '32R',
+            title: 'アナと雪の女王',
+            description: 'string',
+            page_content: 'string',
+            enable_vote: true,
+            twitter_url: 'https://twitter.com/HibiyaSanro',
+            instagram_url: 'https://www.instagram.com/hibiyasanro/',
+            stream_url: 'example.com',
+            id: 'string',
+            thumbnail: '/image/Top-Gun.jpg',
+          },
+           {
+            groupname: '33R',
+            title: 'アナと雪の女王',
+            description: 'string',
+            page_content: 'string',
+            enable_vote: true,
+            twitter_url: 'https://twitter.com/HibiyaSanro',
+            instagram_url: 'https://www.instagram.com/hibiyasanro/',
+            stream_url: 'example.com',
+            id: 'string',
+            thumbnail: '/image/Top-Gun.jpg',
+          },
+          {
+            groupname: '34R',
+            title: 'アナと雪の女王',
+            description: 'string',
+            page_content: 'string',
+            enable_vote: true,
+            twitter_url: 'https://twitter.com/HibiyaSanro',
+            instagram_url: 'https://www.instagram.com/hibiyasanro/',
+            stream_url: 'example.com',
+            id: 'string',
+            thumbnail: '/image/Top-Gun.jpg',
+          },
+           {
+            groupname: '35R',
             title: 'アナと雪の女王',
             description: 'string',
             page_content: 'string',
