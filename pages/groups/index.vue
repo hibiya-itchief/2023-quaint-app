8fa9a31ae29f95d0dc069f75d5306de89d783d2f
Ekke-kuru2 <71656810+Ekke-kuru2@users.noreply.github.com>
2022-09-05 19:33:13 +0900

    毎回同じ順番で表示されないようにgroupsの配列をランダムな順番にする

--- pages/groups/index.vue
+++ pages/groups/index.vue
@@ -104,6 +104,19 @@ export default {
   .catch((e => {
       error({ statusCode:404,message: e.message })
   }))
+
+  const shuffleArray = (array) => {
+    const cloneArray = [...array]
+    for (let i = cloneArray.length - 1; i >= 0; i--) {
+      let rand = Math.floor(Math.random() * (i + 1))
+      // 配列の要素の順番を入れ替える
+      let tmpStorage = cloneArray[i]
+      cloneArray[i] = cloneArray[rand]
+      cloneArray[rand] = tmpStorage
+    }
+    return cloneArray
+  }
+  res_groups=shuffleArray(res_groups)//毎回同じ順番で表示されないようにgroupsの配列をランダムな順番にする
   
   return {tags:res_tags,groups:res_groups}
   },
