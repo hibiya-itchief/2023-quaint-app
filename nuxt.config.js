bb48dfe5f6e17717096d4855f746e2841d00d779
Ekke-kuru2 <71656810+Ekke-kuru2@users.noreply.github.com>
2022-09-08 03:36:33 +0900

    httpsが必要

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,7 +60,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://oci-sub1.ekke.jp/',
+    baseURL: 'https://oci-sub1.ekke.jp/',
     common:{
       'Accept':'application/json, text/plain, */*'
     },
