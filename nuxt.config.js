876015d3b45738c68b751165795a86bc15f5aafb
Ekke-kuru2 <71656810+Ekke-kuru2@users.noreply.github.com>
2022-09-14 00:10:33 +0900

    メタ情報系を編集

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,7 +60,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:8000/',
+    baseURL: 'https://api.2022.seiryofes.com/',
     common:{
       'Accept':'application/json, text/plain, */*'
     },
@@ -101,7 +101,10 @@ export default {
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en'
+      lang: 'ja'
+    },
+    meta:{
+      nativeUI:true
     }
   },
 
