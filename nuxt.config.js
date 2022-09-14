643a41435b4b9686ac5684c548bd4f36ac6bf0b1
Ekke-kuru2 <71656810+Ekke-kuru2@users.noreply.github.com>
2022-09-14 21:56:29 +0900

    bug fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,7 +60,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://api.2022.seiryofes.com/',
+    baseURL: 'https://seiryofes2022api.azurewebsites.net/',
     common:{
       'Accept':'application/json, text/plain, */*'
     },
@@ -130,6 +130,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    analyze: true
   },
   generate: {
     dir: 'docs'
