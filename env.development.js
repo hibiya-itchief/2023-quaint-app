6e45151d83a639674a80a3445eb542be3940f297
ekkekuru2 <ekke@ekke.jp>
2023-03-02 19:50:32 +0900

    fix: update env variables
    nuxt.config.jsにenv:としてファイルからの値を設定すると環境からの変数は読み込まれず上書きされているようなのでそれに合わせた修正。
    
    また、envファイルにプライベートな値を書かないように警告。SSGする予定なのでそもそも公開可能な値しか扱わないと思う。

--- env.development.js
+++ env.development.js
@@ -1,4 +1,6 @@
+// This file is git-controlled, so don't write a private value
 module.exports = {
+  QUAINT_ENV: 'development',
   AZURE_AD_OPENIDCONFIGURATION:
     'https://login.microsoftonline.com/158e6d17-f3d5-4365-8428-26dfc74a9d27/v2.0/.well-known/openid-configuration',
   AZURE_AD_CLIENTID: '0f2252c5-62ef-4aab-af65-99a753f1c77c',
