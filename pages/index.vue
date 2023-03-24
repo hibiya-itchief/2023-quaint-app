9a25b6ca9ee72dcb6869532270c0b06b893e6e28
ekkekuru2 <ekke@ekke.jp>
2023-03-24 23:03:35 +0900

    fix: ログインしてるのにユーザーがフェッチされてないときにフェッチする処理を移動

--- pages/index.vue
+++ pages/index.vue
@@ -9,13 +9,5 @@ import Vue from 'vue'
 export default Vue.extend({
   name: 'IndexPage',
   auth: false,
-  mounted() {
-    if (this.$auth.loggedIn && this.$auth.user?.sub === undefined) {
-      const base64Url = (this.$auth.strategy as any).token.get().split('.')[1] // https://auth.nuxtjs.org/api/auth/#:~:text=Token%20and%20Refresh%20Token%20are%20available%20on%20%24auth.strategy.token
-      const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
-      this.$auth.setUser(JSON.parse(decodeURIComponent(window.atob(base64))))
-    }
-  },
-  methods: {},
 })
 </script>
