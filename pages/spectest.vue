12b99e9d55a216157d03798b21117c2be87ecc4e
とあふ <128669695+hibiyahibiyahibiya@users.noreply.github.com>
2023-04-14 23:37:52 +0900

    Update spectest.vue

--- pages/spectest.vue
+++ pages/spectest.vue
@@ -1,1 +1,50 @@
-
+<template>
+  <v-app>
+    <p>
+      【開発環境】一般客アカウントは、お持ちのメールアドレスで作れるので適宜作って試してください。学校関係者アカウント情報(管理者権限も含む)はDiscordで配布します。
+    </p>
+    <v-row justify="center" class="ma-4">
+      <v-col cols="11" md="6" lg="4" class="ma-0 pa-0">
+        <v-sheet color="white" elevation="4" class="py-4 px-2">
+          <h2 align="center" class="text-h5 ma-3">ログイン</h2>
+          <p align="center" class="">
+            整理券の取得にはログインが必要となります。
+          </p>
+          <v-divider class="my-3"></v-divider>
+          <v-btn
+            class="light-blue darken-4 my-3"
+            block
+            dark
+            @click="loginB2c()"
+          >
+            一般の方はこちらから
+          </v-btn>
+          <p align="center" class="text-caption">
+            アカウントをお持ちでない方も上のボタンを押していただき、指示に従って作成してください。当日入場する際に再ログインが求められる可能性があるため、パスワードを必ず記憶してください。
+          </p>
+          <v-divider class="my-3"></v-divider>
+          <v-btn class="light-blue darken-4 my-3" block dark @click="loginAd()">
+            本校生徒・先生方はこちらから
+          </v-btn>
+          <p align="center" class="text-caption">
+            事前に配布された「@seiryofes.com」で終わるアカウントでログインしてください
+          </p>
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-app>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  mounted() {},
+  methods: {
+    loginB2c() {
+      this.$auth.loginWith('b2c')
+    },
+    loginAd() {
+      this.$auth.loginWith('ad')
+    },
+  },
+})
+</script>
