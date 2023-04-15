6ff263705b7e1d1aad6c905ebf06cd4feaf01ba5
とあふ <128669695+hibiyahibiyahibiya@users.noreply.github.com>
2023-04-15 14:36:19 +0900

    Update spectest.vue

--- pages/spectest.vue
+++ pages/spectest.vue
@@ -8,12 +8,12 @@
           <p    align="center"    class="text-subtitle-1">   指示があるまではどのボタンも押さないでください   </p>
           <v-divider class="my-3"></v-divider>
           
-          <v-btn		class="light-blue darken-4 my-8"		block		rounded="xl"		dark		@click="loginB2c()">    ボタン１    </v-btn>
-          <v-btn		class="light-blue darken-4 my-8"		block		rounded="xl"		dark		@click="loginB2c()">		ボタン２		</v-btn>
-					<v-btn		class="light-blue darken-4 my-8"		block		rounded="xl"		dark		@click="loginB2c()">    ボタン３    </v-btn>
-          <v-btn		class="light-blue darken-4 my-8"		block		rounded="xl"		dark		@click="loginB2c()">		ボタン４    </v-btn>
-					<v-btn		class="light-blue darken-4 my-8"		block		rounded="xl"		dark		@click="loginB2c()">    ボタン５    </v-btn>
-          <v-btn		class="light-blue darken-4 my-8"		block		rounded="xl"		dark		@click="loginB2c()">		ボタン６    </v-btn>
+          <v-btn		class="light-blue darken-4 my-8"		block		rounded="xl"		dark		@click="spectestBtn1">    ボタン１    </v-btn>
+          <v-btn		class="light-blue darken-4 my-8"		block		rounded="xl"		dark		@click="spectestBtn2">		ボタン２    </v-btn>
+					<v-btn		class="light-blue darken-4 my-8"		block		rounded="xl"				@click="spectestBtn3">    ボタン３    </v-btn>
+          <v-btn		class="light-blue darken-4 my-8"		block		rounded="xl"				@click="spectestBtn4">		ボタン４    </v-btn>
+					<v-btn		class="light-blue darken-4 my-8"		block		rounded="xl"		dark		@click="spectestBtn5">    ボタン５    </v-btn>
+          <v-btn		class="light-blue darken-4 my-8"		block		rounded="xl"		dark		@click="spectestBtn6">		ボタン６    </v-btn>
           <p align="center" class="text-subtitle-1">   IT委員会   </p>
           
         </v-sheet>
@@ -33,6 +33,12 @@ export default Vue.extend({
     loginAd() {
       this.$auth.loginWith('ad')
     },
+    spectestBtn1()    {console.log("整理券を1人1枚取得")}
+    spectestBtn2()    {console.log("整理券を1人1枚取得し、リアルタイム混雑状況も確認")}
+    spectestBtn3()    {console.log("整理券を1人1枚取得し、混雑状況も確認、APIの/groupsのエンドポイントも呼ぶ")}
+    spectestBtn4()    {console.log("1端末から3人分の負荷をかける")}
+    spectestBtn5()    {console.log("1端末から5人分の負荷をかける")}
+    spectestBtn6()    {console.log("1端末から10人分の負荷をかける")}
   },
 })
 </script>
