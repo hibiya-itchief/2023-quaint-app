c2557809d6cb768e6298bd22d680364ebf5b8cd6
とあふ <128669695+hibiyahibiyahibiya@users.noreply.github.com>
2023-04-15 14:08:16 +0900

    Update spectest.vue

--- pages/spectest.vue
+++ pages/spectest.vue
@@ -3,20 +3,18 @@
     <v-row justify="center" class="ma-4">
       <v-col cols="11" md="6" lg="4" class="ma-0 pa-0">
         <v-sheet color="white" elevation="4" class="py-4 px-2">
+        
           <h2 align="center" class="text-h5 ma-3">負荷テスト</h2>
           <v-divider class="my-3"></v-divider>
           
-          <v-btn		class="light-blue darken-4 my-3"		block		rounded="xl"		dark		@click="loginB2c()">ボタン１</v-btn>
-          <v-btn		class="light-blue darken-4 my-6"		block		rounded="xl"		dark		@click="loginB2c()">		ボタン２		</v-btn>
-					
-          <v-divider class="my-3"></v-divider>
-          <v-chip class="light-blue darken-4 my-3" block dark @click="loginAd()">
-            簡単なぼたん５
-          </v-chip>
-
-          <p align="center" class="text-caption">
-            事前に配布された「空きテナント６」
-          </p>
+          <v-btn		class="light-blue darken-4 my-5"		block		rounded="xl"		dark		@click="loginB2c()">    ボタン１    </v-btn>
+          <v-btn		class="light-blue darken-4 my-5"		block		rounded="xl"		dark		@click="loginB2c()">		ボタン２		</v-btn>
+					<v-btn		class="light-blue darken-4 my-5"		block		rounded="xl"		dark		@click="loginB2c()">    ボタン３    </v-btn>
+          <v-btn		class="light-blue darken-4 my-5"		block		rounded="xl"		dark		@click="loginB2c()">		ボタン４    </v-btn>
+					<v-btn		class="light-blue darken-4 my-5"		block		rounded="xl"		dark		@click="loginB2c()">    ボタン５    </v-btn>
+          <v-btn		class="light-blue darken-4 my-5"		block		rounded="xl"		dark		@click="loginB2c()">		ボタン６    </v-btn>
+          <p align="center" class="text-caption">   指示があるまではどのボタンも押さないでください   </p>
+          
         </v-sheet>
       </v-col>
     </v-row>
