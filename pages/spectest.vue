c8327c3bec87ffa4c7f2a6d0544f1ecae9644d45
とあふ <128669695+hibiyahibiyahibiya@users.noreply.github.com>
2023-04-15 14:11:08 +0900

    Update spectest.vue

--- pages/spectest.vue
+++ pages/spectest.vue
@@ -4,16 +4,17 @@
       <v-col cols="11" md="6" lg="4" class="ma-0 pa-0">
         <v-sheet color="white" elevation="4" class="py-4 px-2">
         
-          <h2 align="center" class="text-h5 ma-3">負荷テスト</h2>
+          <h2   align="center"    class="text-h5 ma-3">   負荷テスト   </h2>
+          <p    align="center"    class="text-caption">   指示があるまではどのボタンも押さないでください   </p>
           <v-divider class="my-3"></v-divider>
           
-          <v-btn		class="light-blue darken-4 my-5"		block		rounded="xl"		dark		@click="loginB2c()">    ボタン１    </v-btn>
-          <v-btn		class="light-blue darken-4 my-5"		block		rounded="xl"		dark		@click="loginB2c()">		ボタン２		</v-btn>
-					<v-btn		class="light-blue darken-4 my-5"		block		rounded="xl"		dark		@click="loginB2c()">    ボタン３    </v-btn>
-          <v-btn		class="light-blue darken-4 my-5"		block		rounded="xl"		dark		@click="loginB2c()">		ボタン４    </v-btn>
-					<v-btn		class="light-blue darken-4 my-5"		block		rounded="xl"		dark		@click="loginB2c()">    ボタン５    </v-btn>
-          <v-btn		class="light-blue darken-4 my-5"		block		rounded="xl"		dark		@click="loginB2c()">		ボタン６    </v-btn>
-          <p align="center" class="text-caption">   指示があるまではどのボタンも押さないでください   </p>
+          <v-btn		class="light-blue darken-4 my-8"		block		rounded="xl"		dark		@click="loginB2c()">    ボタン１    </v-btn>
+          <v-btn		class="light-blue darken-4 my-8"		block		rounded="xl"		dark		@click="loginB2c()">		ボタン２		</v-btn>
+					<v-btn		class="light-blue darken-4 my-8"		block		rounded="xl"		dark		@click="loginB2c()">    ボタン３    </v-btn>
+          <v-btn		class="light-blue darken-4 my-8"		block		rounded="xl"		dark		@click="loginB2c()">		ボタン４    </v-btn>
+					<v-btn		class="light-blue darken-4 my-8"		block		rounded="xl"		dark		@click="loginB2c()">    ボタン５    </v-btn>
+          <v-btn		class="light-blue darken-4 my-8"		block		rounded="xl"		dark		@click="loginB2c()">		ボタン６    </v-btn>
+          <p align="center" class="text-caption">   IT委員会   </p>
           
         </v-sheet>
       </v-col>
