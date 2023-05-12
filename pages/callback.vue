936790af9a38c7d0f167463658fb1e8d2ae38dbc
とあふ <128669695+hibiyahibiyahibiya@users.noreply.github.com>
2023-05-12 17:43:38 +0900

    Update callback.vue

--- pages/callback.vue
+++ pages/callback.vue
@@ -1,7 +1,14 @@
 <template>
-  <h1>ログインしています...</h1>
-</template>
-<script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({})
-</script>
+    <h1>ログインしています...</h1>
+  </template>
+  <script lang="ts">
+  import Vue from 'vue'
+  export default Vue.extend({
+    created(){
+        if( this.$route.query.error !== null){
+            this.$router.push(`/login`)
+    }
+    }
+  })
+  </script>
+  
