d776a5c587f0966ea157f8e4541c5605141ab31f
とあふ <128669695+hibiyahibiyahibiya@users.noreply.github.com>
2023-06-09 00:00:58 +0900

    Update index.vue

--- pages/groups/index.vue
+++ pages/groups/index.vue
@@ -113,7 +113,6 @@ export default Vue.extend({
         else if( group.description!==null && group.description.includes(this.search_query) ){ return true } 
       }
       else if ( group.tags.some( (i) => i.id === this.selectedTag?.id ) ) { return true }
-      this.search_result_number =7
       else{return false}
     },// tag全体（{id:hogehoge, tagname:honyohonyo}の形）を用いると，tagが一致している判定がうまく行えなかったので，idを用いてtagの一致を判定している
 
