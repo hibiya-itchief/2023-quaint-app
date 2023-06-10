fb5ec630ba548056e869fde04c9fecb4b19e8bb9
とあふ <128669695+hibiyahibiyahibiya@users.noreply.github.com>
2023-06-10 14:23:59 +0900

    Update index.vue

--- pages/groups/_groupId/index.vue
+++ pages/groups/_groupId/index.vue
@@ -56,10 +56,10 @@
                 ><v-icon>mdi-instagram</v-icon></v-btn
               >
               <v-spacer></v-spacer>
-              <v-btn v-if="!IsFavorite" @click="addFavorite(group)" icon><v-icon>mdi-star-outline</v-icon></v-btn>
-              <v-btn v-if="IsFavorite" @click="removeFavorite(group)" icon class="yellow--text"
+              <v-btn v-if="IsFavorite" @click="addFavorite(group)" icon class="yellow--text"
                 ><v-icon>mdi-star</v-icon></v-btn
               >
+              <v-btn v-else @click="addFavorite(group)" icon><v-icon>mdi-star-outline</v-icon></v-btn>
               <p class="ma-0 pa-0 text--caption">お気に入り機能(試行中)</p>
             </v-card-actions>
 
