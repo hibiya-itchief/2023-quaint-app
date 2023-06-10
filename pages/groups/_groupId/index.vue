00bc4d1643ab5354f6eccd26516bab142d933b70
とあふ <128669695+hibiyahibiyahibiya@users.noreply.github.com>
2023-06-10 14:51:00 +0900

    Update index.vue

--- pages/groups/_groupId/index.vue
+++ pages/groups/_groupId/index.vue
@@ -358,10 +358,10 @@ export default Vue.extend({
 
   methods: {
     IsFavorite(group: Group){
-      if(this.changeFavorite == false ){ this.changeFavorite = true; return false};
+      if(this.changeFavorite == false ){ this.changeFavorite = true; return false}
       for(let i = 0; i < localStorage.length; i++){
         if ( group?.id == localStorage.key(i) ){ return true }
-      };
+      }
       return false
     },
     addFavorite(group: Group){
