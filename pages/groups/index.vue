96a75aaf40d29e91aafcdafcc973e3af58b25acf
とあふ <128669695+hibiyahibiyahibiya@users.noreply.github.com>
2023-07-10 09:08:28 +0900

    お気に入り機能修正（groups.length→localStorage.length）

--- pages/groups/index.vue
+++ pages/groups/index.vue
@@ -163,7 +163,7 @@ export default Vue.extend({
           return true
         }
       } else if (this.selectedTag === null) {
-        for (let i = 0; i < this.groups.length; i++) {
+        for (let i = 0; i < localStorage.length; i++) {
           if ('seiryofes.groups.favorite.' + group.id === localStorage.key(i)) {
             return true
           }
