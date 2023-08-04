2550f388fa262a0bfa4c636f77f8cea07f809862
ekkekuru2 <ekke@ekke.jp>
2023-08-04 23:26:33 +0900

    fix: ci warnigを修正 async fetch()使ってなかったので削除

--- layouts/default.vue
+++ layouts/default.vue
@@ -183,7 +183,6 @@ export default Vue.extend({
       ],
     }
   },
-  async fetch() {},
   mounted() {
     if (this.$auth.loggedIn && this.$auth.user?.sub === undefined) {
       const base64Url = (this.$auth.strategy as any).token.get().split('.')[1] // https://auth.nuxtjs.org/api/auth/#:~:text=Token%20and%20Refresh%20Token%20are%20available%20on%20%24auth.strategy.token
