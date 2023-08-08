73f329bd7bc6475daa3eff3eb7d80c52b87b76d0
KT <127566696+TK-7728@users.noreply.github.com>
2023-08-08 15:05:41 +0900

    Update index.vue

--- pages/vote/index.vue
+++ pages/vote/index.vue
@@ -1,1 +1,14 @@
+<template>
+  <v-app>
+    <v-container name="vote-container">
+      <v-row justify="center" align-content="center">
+        <v-col cols="12" sm="6" lg="6">
+          <!--再読み込みボタン-->
+          <div class="text-center pa-1">
+            <v-btn class="mx-1 my-1" color="primary" @click="fetchTicket()"
+              ><v-icon>mdi-reload</v-icon>再読み込み</v-btn
+            >
+          </div>
 
+          
+          
