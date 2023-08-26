95510860728b0860e958394ee940de21ef3e0da8
ekkekuru2 <ekke@ekke.jp>
2023-08-26 22:02:50 +0900

    feat: /admin リンクを追加

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,21 +1,46 @@
 <template>
   <v-app>
-    <h1>管理者用画面</h1>
-    <p>
-      一応現段階でも<a :href="apiUrl + 'docs'">{{ apiUrl }}docs</a
-      >にアクセスするとすべての機能にアクセスできるが、使いづらい
-    </p>
-    <h2>この画面でできるようにしたいこと</h2>
-    <p>新規団体の追加</p>
-    <p>ユーザーの権限管理</p>
-    <p>
-      チーフ会やIT委員会が、複数の団体の複数の公演を条件に合わせて一括で処理できるようにする。例)全クラスの第一公演の開始時刻を一括で15分遅らせる
-    </p>
-    <p>チーフ会・IT委員会のみが確認できる情報にアクセスする。例)投票数</p>
+    <v-row justify="center">
+      <v-col cols="12" md="8" lg="6">
+        <h1>管理者用画面</h1>
+        <h3>
+          <a :href="apiUrl + 'docs'">{{ apiUrl }}docs</a>
+        </h3>
+        <p>quaint-apiのAPIドキュメント、およびSwagger-ui</p>
+        <h3>
+          <a
+            href="https://analytics.google.com/analytics/web/?authuser=1#/p332029403/reports/reportinghub"
+            >Google Analytics</a
+          >
+        </h3>
+        <p>
+          サイトのアクセス解析。Googleアカウントでログインすると、アクセス解析が見れるようになります。
+        </p>
+        <h3>
+          <a href="https://portal.azure.com/signin/index/@seiryofes.com"
+            >Azure ディレクトリ - @seiryofes.com(Azure AD)</a
+          >
+        </h3>
+        <p>
+          quaintのユーザーを管理しているAzure
+          ADのディレクトリ。学校から配布されているアカウントをインポートして用いたり、IT委員会側から配布するアカウントを管理したりしている。
+        </p>
+        <h3>
+          <a
+            href="https://portal.azure.com/signin/index/@seiryofesb2c.onmicrosoft.com"
+            >Azure ディレクトリ - @seiryofesb2c.onmicrosoft.com(Azure B2C)</a
+          >
+        </h3>
+        <p>
+          quaintのユーザーを管理しているAzure
+          B2Cのディレクトリ。一般客が自分のメールアドレスでサインアップするアカウントを管理している。
+        </p>
 
-    <v-btn color="primary" @click="updateFrontend"
-      >フロントエンドをビルドして最新の状態に更新</v-btn
-    >
+        <v-btn color="primary" @click="updateFrontend"
+          >フロントエンドをビルドして最新の状態に更新</v-btn
+        >
+      </v-col>
+    </v-row>
   </v-app>
 </template>
 <script>
