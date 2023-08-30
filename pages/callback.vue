0022c04eb91336621ec78234db22bb194892e988
とあふ <128669695+hibiyahibiyahibiya@users.noreply.github.com>
2023-08-30 13:06:26 +0900

    /callback 祭徠仕様に変更(簡素)

--- pages/callback.vue
+++ pages/callback.vue
@@ -1,12 +1,37 @@
 <template>
-  <h1>ログインしています...</h1>
+  <v-app dark>
+    <v-row justify="center" align-content="center">
+      <p style="font-size: 150%">読み込み中…</p>
+    </v-row>
+    <v-row justify="center">
+      <v-progress-circular
+        v-if="$vuetify.breakpoint.xs"
+        indeterminate
+        color="sairai"
+        size="100"
+        width="8"
+      >
+      </v-progress-circular>
+      <v-progress-circular
+        v-else
+        indeterminate
+        color="sairai"
+        size="150"
+        width="10"
+      >
+      </v-progress-circular>
+    </v-row>
+  </v-app>
 </template>
+
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
   created() {
     if (this.$route.query.error !== undefined) {
       this.$router.push(`/login?error=loginerror`)
+    } else if (this.$route.query.code === undefined) {
+      this.$nuxt.error({ statusCode: 404, message: 'Not Found' })
     }
   },
 })
