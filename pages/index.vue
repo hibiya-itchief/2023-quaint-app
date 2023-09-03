5253fd148fd6d3dc8074b9ec6e4d58ba49762a68
とあふ <128669695+hibiyahibiyahibiya@users.noreply.github.com>
2023-09-03 15:01:27 +0000

    accessをトップページに移管

--- pages/index.vue
+++ pages/index.vue
@@ -35,28 +35,62 @@
         </v-card>
       </v-col>
 
-      <v-col cols="10" md="5" sm="10"
-        ><v-card class="my-auto" to="/access">
-          <div>
-            <v-card-title
-              ><v-icon>mdi-train</v-icon>本校へのアクセス</v-card-title
-            >
-          </div>
-        </v-card></v-col
-      >
+      <v-container>
+        <v-row justify="center">
+          <v-col cols="11">
+            <h1 id="access-title">アクセス</h1>
+            <v-row>
+              <v-col cols="12" sm="6" md="6">
+                <v-img
+                  class="access-img"
+                  src="/images/accessMap.png"
+                  alt="周辺の地図"
+                />
+              </v-col>
+              <v-col cols="12" sm="6" md="6">
+                <h2 id="access-subtitle">ご来場方法</h2>
+                <br />
+                <p>ご来場の際には以下の最寄り駅が使用できます。</p>
+                <p class="access-nagata">
+                  永田町駅 ▶ 有楽町線 / 半蔵門線 / 南北線
+                </p>
+                <p class="access-nagata-indent">
+                  6番出口より参議院議長公邸を右手に、1つ目の交差点を右に曲がり、そのまま道に沿ってお進み下さい。
+                </p>
+                <p class="access-mitsuke">赤坂見附駅 ▶ 銀座線 / 丸ノ内線</p>
+                <p class="access-mitsuke-indent">
+                  11番出口よりプルデンシャルタワー横の新坂（遅刻坂）をお上り下さい。
+                </p>
+                <p class="access-mitsuke-indent">
+                  ※
+                  傾斜がきついため、ご年配の方やお子様連れの方は永田町駅側からお越しください。
+                </p>
+                <p class="access-gijido">
+                  国会議事堂前駅 ▶ 丸ノ内線 / 千代田線
+                </p>
+                <p class="access-gijido">（溜池山王駅） ▶ 銀座線 / 南北線</p>
+                <p class="access-gijido-indent">
+                  ※
+                  <span class="access-attention"
+                    >溜池山王駅・国会議事堂駅側にある正門からの入場はできません</span
+                  >。
+                </p>
+                <p class="access-gijido-indent">
+                  5番出口より地図上の迂回路を通り、
+                </p>
+                <p class="access-gijido-indent">
+                  通用門（永田町駅・赤坂見附駅側）へお回りください。
+                </p>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-container>
+
       <v-col cols="10" md="5" sm="10"
         ><v-card class="my-auto" to="/groups">
           <div>
             <v-card-title><v-icon>mdi-magnify</v-icon>公演一覧</v-card-title>
-          </div>
-        </v-card></v-col
-      >
-      <v-col cols="10" md="5" sm="10"
-        ><v-card class="my-auto" to="/groups">
-          <div>
-            <v-card-title
-              ><v-icon>mdi-map-clock</v-icon>タイムテーブル</v-card-title
-            >
           </div>
         </v-card></v-col
       >
@@ -167,35 +201,58 @@ export default Vue.extend({
 })
 </script>
 
-<style>
+<style scoped>
 body {
   margin: 0;
   padding: 0;
 }
 
-.dot {
-  color: #f6bd60;
+#access-title {
+  display: inline-block;
+  padding: 0.5rem 3rem 0.5rem 0;
+  margin-bottom: 5rem;
+  border-bottom: 3px solid #b49656;
+  font-family: serif;
+  font-weight: bold;
 }
 
-.coming-soon {
-  display: block;
-  margin: 0;
-  padding: 0;
-  font-size: 7vw;
-  font-family: 'Josefin Sans', sans-serif;
+#access-subtitle {
+  text-align: center;
+  font-weight: normal;
 }
 
-.feedback {
-  display: block;
-  font-size: 11px;
-  margin-top: 50px;
+.access-img {
+  border: 9px solid #b49656;
 }
 
-ion-icon {
-  display: block;
-  margin: 3vw 0;
-  font-size: 4vw;
-  text-decoration: none;
-  color: #000;
+.access-attention {
+  color: #b49656;
+  font-weight: bold;
+  text-decoration: underline;
+}
+
+.access-nagata {
+  margin-bottom: 0;
+}
+
+.access-nagata-indent {
+  margin-left: 5.5rem;
+}
+
+.access-mitsuke {
+  margin-bottom: 0;
+}
+
+.access-mitsuke-indent {
+  margin-left: 6.5rem;
+}
+
+.access-gijido {
+  margin-bottom: 0;
+}
+
+.access-gijido-indent {
+  margin-left: 8.5rem;
+  margin-bottom: 0;
 }
 </style>
